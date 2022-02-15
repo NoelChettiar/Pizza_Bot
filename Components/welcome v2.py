45a09cb72b18e7e50c0bbca12eee558fecd18e3b
NoelChettiar <99223010+NoelChettiar@users.noreply.github.com>
2022-02-16 09:26:26 +1300

    Added Welcome Function
    Added welcome and main function from component 1 version 2

--- Components/welcome v2.py
+++ Components/welcome v2.py
@@ -19,7 +19,6 @@ def welcome():
     print("*** I will be here to help you order your delicious Dream Pizza ***")
 
 
-
 def main():
     
     '''
@@ -30,4 +29,4 @@ def main():
     '''
     welcome()
 
-main()
+    main()
--- pizza_bot.py
+++ pizza_bot.py
@@ -1,12 +1,24 @@
 #Pizza Bot Program
+import random
+from random import randint
+
+#List of random names
+names = ["James", "Phil", "Emma", "Philip", "Jacob", "Jayden", "Jill", "Ellen", "Laura","Eric" ]
 
 #Welcome message with random name
-
-
-
-
-
-
+def welcome():
+    '''
+    Purpose: To generate a random name from the list and print out
+    a welcome message
+    Parameters: None
+    Returns: None
+    '''
+    
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to Dream Pizza ***")
+    print("*** My name is",name, "***")
+    print("*** I will be here to help you order your delicious Dream Pizza ***")
 #Menu for pickup delivery
 
 
@@ -67,4 +79,14 @@
 
 
 #Main Function
+def main():
+    
+    '''
+    Purpose: To run all functions
+    a welcome message
+    Parameters: None
+    Returns:None
+    '''
+    welcome()
 
+    main()
